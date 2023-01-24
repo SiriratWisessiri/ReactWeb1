3a7f63f2a0c55bc15f8680748a5d5d3b0f30344f
Sirirat Wisessiri <sirirat.wisessiri@avanade.com>
2023-01-24 15:55:37 +1100

    Initial complete of Hero section and added style

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar/>
         <Routes>
-          <Route path='/' exact />
+          {/* <Route path='/' exact component={Home}/> */}
+          <Route exact path="/" element={<Home />}/>
         </Routes>
       </div>
     </Router>
