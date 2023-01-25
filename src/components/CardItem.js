ab3122283067d6db02cd427c717394e17b4c6123
Sirirat Wisessiri <sirirat.wisessiri@avanade.com>
2023-01-25 12:02:11 +1100

    Fixed card's CSS

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function CardItem() {
+function CardItem(props) {
   return (
-    <div>
-        <li className='cards_items'>
-            <Link className='cards_item_link'>
-                <figure className='cards_item_pic-wrap'>
-                    <img src='/' alt="Travel image" className='cards_item_img'/>
-                </figure>
-                <div className='cards_item_info'>
-                    <h5 className='cards_item_text'></h5>
-                </div>
-            </Link>
-        </li>
-    </div>
-  )
+    <>
+   <li className='cards_item'>
+         <Link className='cards_item_link' to={props.path}>
+           <figure className='cards_item_pic-wrap' data-category={props.label}>
+             <img
+               className='cards_item_img'
+               alt='Travel Image'
+              src={props.src}
+             />
+           </figure>
+           <div className='cards_item_info'>
+             <h5 className='cards_item_text'>{props.text}</h5>
+           </div>
+         </Link>
+       </li>
+    </>
+  );
 }
 
 export default CardItem
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,16 +1,48 @@
 import React from 'react'
 import CardItem from './CardItem'
 import './Card.css'
-
+// This file contains cards compenent that wil be pass as props in CardItem.js
 function Cards() {
   return (
     <div className='cards'>
         <h1>Check out these EPIC destinations</h1>
         <div className='cards_container'>
             <div className='cards_wrapper'>
+                {/* The below ul will be pass as a props in CardItem.js */}
                 <ul className='cards_items'>
-                    <CardItem/>
+                    <CardItem src = 'images/img-9.jpg'
+                    text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                    label='Adventure'
+                    path='/services' 
+                    />
+
+                    <CardItem src = 'images/img-2.jpg'
+                    text='Travel to the Islands of Bali in a private cruise'
+                    label='Luxury'
+                    path='/services' 
+                    />
                 </ul>
+                <ul className='cards_items'>
+                    <CardItem src = 'images/img-8.jpg'
+                    text='Ride through the Sahara Desert on a guided camel tour'
+                    label='Mystery'
+                    path='/services' 
+                    />
+
+                    <CardItem src = 'images/img-1.jpg'
+                    text='Travel to the Islands of Bali in a private cruise'
+                    label='Adventure'
+                    path='/services' 
+                    />
+
+                    <CardItem
+                    src='images/img-4.jpg'
+                    text='Experience Football on Top of the Himilayan Mountains'
+                    label='Adventure'
+                    path='/products'
+                    />
+                </ul>
+                {/*  */}
             </div>
         </div>
     </div>
@@ -18,3 +50,4 @@ function Cards() {
 }
 
 export default Cards
+
